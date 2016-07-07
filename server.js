266be6db06cedf64b366ad7f1d82c98311ebd20c
Julien Michelot <jmichelot@yahoo.com>
2016-07-07 11:24:46 +0200

    heroku port test

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 
 // var mongoUrl = 'mongodb://localhost:27017/clementinejs';
 var mongoUrl = 'mongodb://devuser:devuser@ds015995.mlab.com:15995/fcc';
+var port = process.env.PORT || 3000;
 
 mongo.connect(mongoUrl, function (err, db) {
    if (err) throw new Error('Database failed to connect!');
@@ -18,8 +19,8 @@ mongo.connect(mongoUrl, function (err, db) {
 
    routes(app, db);
 
-   app.listen(3000, function () {
-      console.log('Node.js listening on port 3000...');
+   app.listen(port, function () {
+      console.log('Node.js listening on port ' + port + '...');
    });
 
 });
