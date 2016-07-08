ae92e8b3d71df951a69462f8be13e95ddfe95033
Julien Michelot <jmichelot@yahoo.com>
2016-07-08 10:33:59 +0200

    secret keys added

--- server.js
+++ server.js
@@ -3,24 +3,24 @@
 var express = require('express');
 var mongo = require('mongodb');
 var routes = require('./app/routes/index.js');
+var config = require('./config.js');
 
 var app = express();
 
-// var mongoUrl = 'mongodb://localhost:27017/clementinejs';
-var mongoUrl = 'mongodb://devuser:devuser@ds015995.mlab.com:15995/fcc';
-var port = process.env.PORT || 3000;
+var mongoUrl = process.env.MONGODB_URI || config.mongoUri;
+var port = process.env.PORT || config.serverPort;
 
 mongo.connect(mongoUrl, function (err, db) {
-   if (err) throw new Error('Database failed to connect!');
-   else console.log('Successfully connected to MongoDB.');
+  if (err) throw new Error('Database failed to connect!');
+  else console.log('Successfully connected to MongoDB.');
 
-   app.use('/public', express.static(process.cwd() + '/public'));
-   app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+  app.use('/public', express.static(process.cwd() + '/public'));
+  app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-   routes(app, db);
+  routes(app, db);
 
-   app.listen(port, function () {
-      console.log('Node.js listening on port ' + port + '...');
-   });
+  app.listen(port, function () {
+    console.log('Node.js listening on port ' + port + '...');
+  });
 
 });
