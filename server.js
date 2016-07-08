bd6ff374fcf2a96dc9efa8e10b7da07ab01fca99
Julien Michelot <jmichelot@yahoo.com>
2016-07-08 11:52:13 +0200

    v1.0

--- server.js
+++ server.js
@@ -13,17 +13,38 @@ var app = express();
 var mongoUrl = process.env.MONGODB_URI || config.mongoUri;
 var port = process.env.PORT || config.serverPort;
 
+var result = { 'unix': null, 'natural': null };
+
 mongo.connect(mongoUrl, function (err, db) {
   if (err) throw new Error('Database failed to connect!');
   else console.log('Successfully connected to MongoDB.');
 
   app.use('/public', express.static(process.cwd() + '/public'));
-  app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-  routes(app, db);
+  app.param('date', function (req, res, next, value) {
+    var date;
+    result.unix = null;
+    result.natural = null;
+    // is value a number ?
+    if (!Number.isNaN(+value)) date = new Date(+value);
+    else date = new Date(value);
+    // is date a valid Date ?
+    if (!Number.isNaN(date.valueOf())) {
+      result.unix = date.valueOf();
+      result.natural = value;
+    } 
+    next();
+  });
+
+  app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/public/index.html');
+  });
+
+  app.get('/:date', function (req, res, next) {
+    res.json(result);
+  });
 
   app.listen(port, function () {
     console.log('Node.js listening on port ' + port + '...');
   });
-
 });
